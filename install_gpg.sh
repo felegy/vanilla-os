aacd8914e88d8805cd6b875685ea0d822f59eb8b
Gabor FELEGYHAZI <1136546+felegy@users.noreply.github.com>
2024-12-28 12:53:37 +0100

    Update install_gpg.sh

--- install_gpg.sh
+++ install_gpg.sh
@@ -1,3 +1,3 @@
-curl -sLO --output-dir /tmp https://gist.github.com/felegy/2575781644a8c179883be28c9f3bc7b6/raw/34075e8bbf733cf44c3fd747b637430d399212bb/gnupg-2.4.3.tgz
+curl -sLO --output-dir /tmp https://github.com/felegy/vanilla-os/raw/refs/heads/main/gnupg-2.4.6.tgz
 cd /
-tar zxfv /tmp/gnupg-2.4.3.tgz
+tar zxfv /tmp/gnupg-2.4.6.tgz
