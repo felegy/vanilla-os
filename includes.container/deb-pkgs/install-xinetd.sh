7bb386b53c7223cac6fcfd2217747504b0c9520c
felegy <felegy@hvg.hu>
2025-01-08 16:29:54 +0100

    + ADD install-xinetd

---
+++ includes.container/deb-pkgs/install-xinetd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+tee /etc/xinetd.d/vscode-http <<EOF
+service vscode-http
+{
+    type            = UNLISTED
+    port            = 80
+    bind            = 127.0.0.1
+    protocol        = tcp
+    socket_type     = stream
+    wait            = no
+    user            = nobody
+    redirect        = 127.0.0.1 6780
+}
+EOF
+
+tee /etc/xinetd.d/vscode-https <<EOF
+service vscode-https
+{
+    type            = UNLISTED
+    port            = 443
+    bind            = 127.0.0.1
+    protocol        = tcp
+    socket_type     = stream
+    wait            = no
+    user            = nobody
+    redirect        = 127.0.0.1 6743
+}
+EOF
